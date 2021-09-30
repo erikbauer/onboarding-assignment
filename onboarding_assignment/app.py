ccbb3a9d2382449e60d7c242c642aa1e8fdffaa0
erikbauer <erik.bauer@billogram.com>
2021-09-30 09:36:04 +0200

    Add reading of csv file

--- onboarding_assignment/app.py
+++ onboarding_assignment/app.py
@@ -1,6 +1,12 @@
 import csv
+import os
 
 def main():
+    with open('invoices.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['name'] + ", " + row['article_name'])
+
     return 
 
 if __name__ == "__main__":
