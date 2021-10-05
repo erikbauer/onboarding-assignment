ec1feb31be7c5952d1a161a4d0b99027bcff1a70
erikbauer <erik.bauer@billogram.com>
2021-10-05 17:00:24 +0200

    Add classes for remote objects

--- onboarding_assignment/app.py
+++ onboarding_assignment/app.py
@@ -15,9 +15,12 @@ headers = {"Authorization": b"Basic " + b64encode(api_user + b":" + api_password
 
 vat = 25
 
+
 class BillogramAPI:
     def __init__(self, api_user: str, api_password: str) -> None:
         self.basic_auth = (api_user, api_password)
+        self.customers = None
+        self.billograms = None
 
 
 class RemoteObject(object):
@@ -27,14 +30,73 @@ class RemoteObject(object):
         self._id_field = id_field
 
 
-class Customer(RemoteObject):
-    def __init__(self) -> None:
+class Address(object):
+    def __init__(self, street_address: str, zipcode: str, city: str) -> None:
+        self.street_address = street_address
+        self.zipcode = zipcode
+        self.city = city
+
+    def get(self):
+        return {
+            "street_address": self.street_address,
+            "zipcode": self.zipcode,
+            "city": self.city,
+        }
+
+
+class Contact(object):
+    def __init__(self, email: str = None, phone: str = None):
+        self.email = email
+        self.phone = phone
+
+    def get(self):
+        return {"email": self.email, "phone": self.phone}
+
+
+class Item(object):
+    def __init__(self, title: str, price: float, vat: int, count: int) -> None:
         super().__init__()
+        self.title = title
+        self.vat = vat
+        self.price = price
+        self.count = count
+
+    def price_excluding_vat(self):
+        return self.price / (1 + self.vat / 100)
+
+    def get(self):
+        return {"title": self.title, "price": self.price, "vat": self.vat, "count": self.count}
+
+
+class Customer(RemoteObject):
+    def __init__(self, name: str, contact: Contact, address: Address) -> None:
+        super().__init__()
+        self.name = name
+        self.contact = contact
+        self.address = address
+        self.send_method = None
+
+        # Determine send method
+        if self.address.email:
+            self.send_method = "Email"
+        elif self.address.phone:
+            self.send_method = "SMS"
+        else:
+            self.send_method = "Letter"
+
+    def get_body(self):
+        return {
+            "customer_no": self.id_field,
+            "name": self.name,
+            "contact": self.contact.get(),
+            "address": self.address.get(),
+        }
 
 
 class Billogram(RemoteObject):
-    def __init__(self) -> None:
+    def __init__(self, customer: Customer) -> None:
         super().__init__()
+        self.customer = customer
 
 
 def create_customer(invoice: Dict) -> Dict:
