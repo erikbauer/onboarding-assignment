f3212af07ac3299b076347c6ff0c644ed5c11fbc
erikbauer <erik.bauer@billogram.com>
2021-10-05 14:01:50 +0200

    Add linting and formatting

--- onboarding_assignment/app.py
+++ onboarding_assignment/app.py
@@ -1,56 +1,65 @@
+from base64 import b64encode
 import csv
-import os
 import httpx
+import os
 from dotenv import load_dotenv
-from base64 import b64encode, encode
-
 from typing import Dict
 
 load_dotenv()
 
-api_user: bytes = bytes(os.getenv("API_USER"), encoding="utf8") 
-api_password: bytes = bytes(os.getenv("API_PASSWORD"), encoding="utf8")
+api_user: str = os.getenv("API_USER")
+api_password: str = os.getenv("API_PASSWORD")
 base_url = "https://sandbox.billogram.com/api/v2"
 
-headers = {
-    "Authorization": b"Basic " + b64encode(api_user + b":" + api_password)
-}
+headers = {"Authorization": b"Basic " + b64encode(api_user + b":" + api_password)}
 
-vat: int = 25
+vat = 25
+
+class BillogramAPI:
+    def __init__(self, api_user: str, api_password: str) -> None:
+        self.basic_auth = (api_user, api_password)
+
+
+class RemoteObject(object):
+    def __init__(self, api: BillogramAPI, url: str, id_field: str) -> None:
+        self._api = api
+        self._url = url
+        self._id_field = id_field
+
+
+class Customer(RemoteObject):
+    def __init__(self) -> None:
+        super().__init__()
+
+
+class Billogram(RemoteObject):
+    def __init__(self) -> None:
+        super().__init__()
+
 
 def create_customer(invoice: Dict) -> Dict:
     # Check if customer already exists
-    response = httpx.get(
-        base_url + "/customer" + "/" + invoice["customer_number"],
-        headers=headers
-    )
+    response = httpx.get(base_url + "/customer" + "/" + invoice["customer_number"], headers=headers)
 
     if response.status_code == 200:
         response_body = response.json()
         customer = response_body["data"]
         print("Found customer: " + str(customer["customer_no"]))
         return customer
-    else: 
+    else:
         # Customer not found. Create an object for a customer
         customer_body = {
             "customer_no": invoice["customer_number"],
             "name": invoice["name"],
-            "contact": {
-                "email": invoice["email"],
-                "phone": invoice["phone_number"]
-            },
+            "contact": {"email": invoice["email"], "phone": invoice["phone_number"]},
             "address": {
                 "street_address": invoice["street_address"],
                 "zipcode": invoice["postal_code"],
-                "city": invoice["city"]
-            }
+                "city": invoice["city"],
+            },
         }
 
-        response = httpx.post(
-            base_url + "/customer",
-            headers=headers,
-            json=customer_body
-        )       
+        response = httpx.post(base_url + "/customer", headers=headers, json=customer_body)
 
         if response.status_code == 200:
             response_body = response.json()
@@ -60,6 +69,7 @@ def create_customer(invoice: Dict) -> Dict:
         else:
             print(response.text)
             exit
+
 
 def create_billogram(invoice: Dict) -> Dict:
     # Determine send method
@@ -71,30 +81,19 @@ def create_billogram(invoice: Dict) -> Dict:
         send_method = "Letter"
 
     # Exclude VAT from article price
-    price: float = float(invoice["article_price"]) / (1 + vat/100)
+    price: float = float(invoice["article_price"]) / (1 + vat / 100)
 
     billogram_body = {
         "invoice_no": invoice["invoice_number"],
-        "customer": {
-            "customer_no": invoice["customer_number"]
-        },
-        "items": [{
-            "title": invoice["article_name"],
-            "price": price,
-            "vat": vat,
-            "count": 1
-        }],
+        "customer": {"customer_no": invoice["customer_number"]},
+        "items": [{"title": invoice["article_name"], "price": price, "vat": vat, "count": 1}],
         "on_success": {
             "command": "send",
             "method": send_method,
-        }
+        },
     }
 
-    response = httpx.post(
-        base_url + "/billogram",
-        headers=headers,
-        json=billogram_body
-    )
+    response = httpx.post(base_url + "/billogram", headers=headers, json=billogram_body)
 
     if response.status_code == 200:
         response_body = response.json()
@@ -105,15 +104,20 @@ def create_billogram(invoice: Dict) -> Dict:
         print(response.text)
         exit()
 
+
 def read_file() -> Dict:
-    with open('invoices.csv', newline='') as csvfile:
+    with open("invoices.csv", newline="") as csvfile:
         invoices = csv.DictReader(csvfile)
-    
     return invoices
-            
+
+
 def main():
     invoices = read_file()
     for invoice in invoices:
         customer = create_customer(invoice)
         billogram = create_billogram(invoice)
     return
+
+
+if __name__ == "__main__":
+    main()
